756a3d616bcdd1d8c53ea8acf2c07571621bc8e3
alanpodolsk <alanpodolsk@yandex.ru>
2023-10-22 21:52:01 +0300

    Feat: stats-server, stats-client, stats-dto. Docker tuned Fixed checkstyle for Test classes

--- ewm/src/test/java/ru/practicum/AppTest.java
+++ ewm/src/test/java/ru/practicum/AppTest.java
@@ -1,11 +1,4 @@
 package ru.practicum;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 }
